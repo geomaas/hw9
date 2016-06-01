6239c22378b00773e727aecae80ac895884ea6b0
geomaas <geoff.maas@gmail.com>
2016-06-01 08:56:45 -0400

    wed morning

--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
 // document.getElementsByClassName('className')
 // alert("What's up bitches?!");
 // chrome.tabs.create({"url": "http://google.com"});
+// based on googles make page redder sample extension
 chrome.browserAction.onClicked.addListener(function(tab) {
   console.log('Turning ' + tab.url + ' green!');
   chrome.tabs.executeScript({
